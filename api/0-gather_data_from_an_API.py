a9abd83fc188868e22f6eefe6c7c22ff30eb63c3
Malek Mayeh <loukatankionline@gmail.com>
2022-12-02 19:58:35 +0100

    todo list

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""returns info about his/her TODO list progress."""
+
+import requests
+
+import sys
+
+if __name__ == "__main__":
+    api_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(api_url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(
+        api_url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = []
+    for task in todos:
+        if task.get("completed") is True:
+            completed.append(task.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    for complete in completed:
+        print("\t {}".format(complete))
